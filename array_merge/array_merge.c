ba5d03649b6aa5e3503372c7b91d1480401da71c
Mackenzie Amoako <amoak017@umn.edu>
2025-10-02 11:24:57 -0500

    started arraymerge

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,9 @@
 #include "array_merge.h"
 #include "../mergesort/mergesort.h"
 
+extern void mergesort(int size, int values[]);
+
+
 int* array_merge(int num_arrays, int* sizes, int** values) {
   // This is obviously broken. It has the right type, though, eh?
   return sizes;
